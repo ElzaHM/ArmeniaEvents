b3fcb68d8d38f9d22d4e17c6bd1e2bb1a0b18b67
Hasmik <asmikasatryan97@gmail.com>
2026-06-01 11:50:35 +0400

    Apply Sign In auth page styling and layout to Sign Up.

--- src/pages/SignUpPage/index.tsx
+++ src/pages/SignUpPage/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Header from '../../components/Header/Header';
+import { FooterContent } from '../../components/home';
 import { SignUpPanel } from '../../components/sign-up';
 import '../../components/home/home.css';
 import '../../components/sign-in/sign-in.css';
@@ -9,11 +11,14 @@ import styles from '../../components/sign-up/SignUpPage.module.css';
 const SignUpPage: React.FC = () => {
   return (
     <div className={`${styles.pageWrapper} auth-page sign-up-page`}>
+      <Header />
       <div className={styles.arcDecoration} />
 
       <main className={styles.mainContent}>
         <SignUpPanel />
       </main>
+
+      <FooterContent />
     </div>
   );
 };
