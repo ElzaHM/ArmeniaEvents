388a56105c604667f667101b0c900c43c15a9172
Elza <elzahovhannisyan7@gmail.com>
2026-06-10 01:37:08 +0400

    fix .env

--- src/main.tsx
+++ src/main.tsx
@@ -4,25 +4,31 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { GoogleOAuthProvider } from '@react-oauth/google';
-
+import { ThemeProvider } from './providers/theme-provider';
 import './index.css';
-
+import { AuthProvider } from './providers/auth-provider';
 import { router } from './routes';
 import { queryClient } from './providers/query-client';
 
 const googleClientId = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 if (!googleClientId) {
+  console.log(import.meta.env);
   throw new Error('Missing VITE_GOOGLE_CLIENT_ID in .env');
 }
 
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId={googleClientId}>
-      <QueryClientProvider client={queryClient}>
-        <RouterProvider router={router} />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </GoogleOAuthProvider>
+    <AuthProvider>
+      <GoogleOAuthProvider clientId={googleClientId}>
+        <QueryClientProvider client={queryClient}>
+          <ThemeProvider>
+            <RouterProvider router={router} />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </ThemeProvider>
+        </QueryClientProvider>
+      </GoogleOAuthProvider>
+    </AuthProvider>
   </StrictMode>,
 );
